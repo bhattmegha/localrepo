19033bafbe6a60869966d1d9e325743ab2a7910e
Megha Bhatt <meghabhatt2608@gmail.com>
2023-10-20 01:02:59 -0400

    updating file

--- Printnumberpattern.java
+++ Printnumberpattern.java
@@ -9,7 +9,7 @@ public class Printnumberpattern {
 
         for(int i=1;i<=n;i++){
             for(int j=1; j<=i;j++){
-                System.out.print(j);
+                System.out.print(j+ " ");
             }
             System.out.println();
         }
