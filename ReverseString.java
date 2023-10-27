45cb8504b2ff0d6f5923fcb387da8299618cacd4
Megha Bhatt <meghabhatt2608@gmail.com>
2023-10-27 00:41:28 -0400

    Reverse a String

---
+++ ReverseString.java
@@ -0,0 +1,23 @@
+package LocalRepo;
+
+public class ReverseString {
+    public static void main(String args[]){
+
+        StringBuilder sb = new StringBuilder("Megha");
+        System.out.println(sb);
+
+        for(int i=0; i<sb.length()/2; i++){
+            int front = i;
+            int back = sb.length()-1-i;
+
+            char frontchar = sb.charAt(front);
+            char backchar = sb.charAt(back);
+
+            sb.setCharAt(front, backchar);
+            sb.setCharAt(back, frontchar);
+
+        }
+        System.out.println(sb);
+    }
+
+}
